1ca0f9c087f124a63fbaed294899ead960ce7238
RicheyHans <ifyoutolerate@gmail.com>
2017-09-28 15:10:04 +0900

    TapLayoutView updated

--- TapLayoutView_170928/app/src/main/java/com/example/mcbud/taplayout_170927/CustomAdapter.java
+++ TapLayoutView_170928/app/src/main/java/com/example/mcbud/taplayout_170927/CustomAdapter.java
@@ -17,12 +17,10 @@ import java.util.List;
  */
 
 public class CustomAdapter extends PagerAdapter {
-
     private static final int COUNT = 4;
     List<View> views;
 
-
-    public CustomAdapter(Context context){
+    public CustomAdapter(Context context) {
         views = new ArrayList<>();
         views.add(new One(context));
         views.add(new Two(context));
@@ -37,23 +35,22 @@ public class CustomAdapter extends PagerAdapter {
 
     @Override
     public Object instantiateItem(ViewGroup container, int position) {
-        View view = container.getFocusedChild();
-        if( view != null){
-            view = views.get(position);
-            container.addView(view);
-        }
+        //View view = container.getFocusedChild();
+        //if( view == null){
+        View view = views.get(position);
+        container.addView(view);
+        //}
         return view;
     }
 
     @Override
     public void destroyItem(ViewGroup container, int position, Object object) {
-        
-        // super.destroyItem(container, position, object);
+        container.removeView((View) object);
+        //super.destroyItem(container, position, object);
     }
 
     @Override
     public boolean isViewFromObject(View view, Object object) {
-        return false;
+        return view == object;
     }
-
 }
