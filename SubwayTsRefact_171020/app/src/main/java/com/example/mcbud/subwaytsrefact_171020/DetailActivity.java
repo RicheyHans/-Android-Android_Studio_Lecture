e33d61d8e5b420b7e8d4cb5a8231befc27b1d41e
RicheyHans <ifyoutolerate@gmail.com>
2017-10-24 03:52:46 +0900

    detailActivity, UpAdapter has been updated

--- SubwayTsRefact_171020/app/src/main/java/com/example/mcbud/subwaytsrefact_171020/DetailActivity.java
+++ SubwayTsRefact_171020/app/src/main/java/com/example/mcbud/subwaytsrefact_171020/DetailActivity.java
@@ -4,10 +4,18 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.TextView;
 
+import com.example.mcbud.subwaytsrefact_171020.Model.JsonClass;
+import com.example.mcbud.subwaytsrefact_171020.Model.RealtimeArrivalList;
+import com.google.gson.Gson;
+
 import org.w3c.dom.Text;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by mcbud on 2017-10-24.
@@ -18,6 +26,7 @@ public class DetailActivity extends AppCompatActivity {
     private String stnText;
     private RecyclerView recyclerViewUp;
     private RecyclerView recyclerViewDown;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -40,19 +49,43 @@ public class DetailActivity extends AppCompatActivity {
         // 상~하행선 리사클러뷰 객체화
         recyclerViewUp = (RecyclerView) findViewById(R.id.recyclerViewUp);
         recyclerViewDown = (RecyclerView) findViewById(R.id.recyclerViewDown);
+        System.out.println("init실행됨!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     }
 
     private void load(){
         new AsyncTask<Void, Void, String>(){
             @Override
             protected String doInBackground(Void... voids) {
-                return null;
+                String str = Remote.getData("http://swopenapi.seoul.go.kr/api/subway/66795758776d6362373442475a666e/json/realtimeStationArrival/1/10/"+stnText);
+                System.out.println(stnText);
+                System.out.println(str);
+                return str;
             }
 
             @Override
-            protected void onPostExecute(String s) {
-                super.onPostExecute(s);
+            protected void onPostExecute(String jsonString) {
+                data = parse(jsonString);
+                setList();
             }
         }.execute();
     }
+
+    private List<RealtimeArrivalList> parse(String string){
+        Gson gson = new Gson();
+        JsonClass jsonClass = gson.fromJson(string, JsonClass.class);
+
+        RealtimeArrivalList[] realtimeArrivalLists = jsonClass.getRealtimeArrivalList();
+
+        List<RealtimeArrivalList> result = Arrays.asList(realtimeArrivalLists);
+
+        return result;
+    }
+
+    List<RealtimeArrivalList> data;
+    private void setList(){
+        UpAdapter upAdapter = new UpAdapter(data, this);
+        recyclerViewUp.setAdapter(upAdapter);
+        recyclerViewUp.setLayoutManager(new LinearLayoutManager(this));
+    }
+
 }
--- SubwayTsRefact_171020/app/src/main/java/com/example/mcbud/subwaytsrefact_171020/UpAdapter.java
+++ SubwayTsRefact_171020/app/src/main/java/com/example/mcbud/subwaytsrefact_171020/UpAdapter.java
@@ -1,33 +1,51 @@
 package com.example.mcbud.subwaytsrefact_171020;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.mcbud.subwaytsrefact_171020.Model.RealtimeArrivalList;
+
+import java.util.List;
 
 /**
  * Created by mcbud on 2017-10-24.
  */
 
-public class UpAdapter extends RecyclerView.Adapter<StationAdapter.Holder> {
-    @Override
-    public StationAdapter.Holder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+public class UpAdapter extends RecyclerView.Adapter<UpAdapter.Holder> {
+    Context context;
+    List<RealtimeArrivalList> data;
+
+    public UpAdapter(List<RealtimeArrivalList> data, Context context){
+        this.data = data;
+        this.context = context;
     }
 
     @Override
-    public void onBindViewHolder(StationAdapter.Holder holder, int position) {
+    public UpAdapter.Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_up, parent, false);
+        return new Holder(view);
+    }
 
+    @Override
+    public void onBindViewHolder(Holder holder, int position) {
+        RealtimeArrivalList ra = data.get(position);
+        holder.textTime.setText(ra.getArvlMsg2());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return data.size();
     }
 
     public class Holder extends RecyclerView.ViewHolder{
-
+        TextView textTime;
         public Holder(View itemView) {
             super(itemView);
+            textTime = itemView.findViewById(R.id.textTime);
         }
     }
 }
